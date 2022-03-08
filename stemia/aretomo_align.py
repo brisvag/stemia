64ae0846a43e6a5513992e49b2c4672ae9bc49af
Lorenzo Gaifas <brisvag@gmail.com>
2022-03-08 15:43:52 +0100

    modular

--- stemia/aretomo_align.py
+++ stemia/aretomo_align.py
@@ -1,81 +1,68 @@
 import click
+import subprocess
+from pathlib import Path
+import re
+import shutil
+import GPUtil
+from inspect import cleandoc
+import mrcfile
 
 
-@click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.argument('warp_dir', type=click.Path(exists=True, dir_okay=True, resolve_path=True), default='.')
-@click.option('-d', '--dry-run', is_flag=True, help='only print some info, without running the commands')
-@click.option('-t', '--tilt-axis', type=str, help='starting tilt axis for AreTomo, if any')
-@click.option('-f', '--overwrite', is_flag=True, help='overwrite any previous existing run')
-@click.option('--ccderaser', type=str, default='ccderaser', help='command for ccderaser')
-@click.option('--aretomo', type=str, default='AreTomo', help='command for aretomo')
-def main(warp_dir, dry_run, ccderaser, aretomo, tilt_axis, overwrite):
-    """
-    run aretomo on a warp directory (after imod stacks were generated).
-    Requires ccderaser and AreTomo.
+def check_outputs_exist(root, names, ext):
+    if not isinstance(ext, tuple):
+        ext = (ext,)
+    for ts in names:
+        dir = root / f'{ts}.mrc'
+        for ex in ext:
+            file = dir / f'{ts}{ext}'
+            if file.exists():
+                raise FileExistsError(f'{file} already exists; use -f/--overwrite to overwrite any existing files')
 
-    Assumes the default Warp directory structure with generated imod stacks
-    """
-    import subprocess
-    from pathlib import Path
-    import re
-    import shutil
-    import GPUtil
-    from inspect import cleandoc
-    import mrcfile
 
+def run_fix(imod_dir, ts_list, overwrite, in_ext, ccderaser):
     if not shutil.which(ccderaser):
         raise click.UsageError('ccderaser is not available on the system')
+
+    if not overwrite:
+        check_outputs_exist(imod_dir, ts_list, '_fixed.mrc')
+    with click.progressbar(ts_list, label='Fixing...     ', item_show_func=str) as bar:
+        for ts_dir in bar:
+            ts_name = ts_dir / ts_dir.stem
+            input = f'{ts_name}{in_ext}'
+            output = f'{ts_name}_fixed.mrc'
+
+            # run ccderaser, defaults from etomo
+            ccderaser_cmd = f'{ccderaser} -input {input} -output {output} -find -peak 8.0 -diff 6.0 -big 19. -giant 12. -large 8. -grow 4. -edge 4'
+            subprocess.run(ccderaser_cmd.split(), capture_output=True, check=True)
+
+
+def run_normalize(imod_dir, ts_list, overwrite, in_ext):
+    if not overwrite:
+        check_outputs_exist(imod_dir, ts_list, '_norm.mrc')
+    with click.progressbar(ts_list, label='Normalizing...', item_show_func=str) as bar:
+        for ts_dir in bar:
+            ts_name = ts_dir / ts_dir.stem
+            input = f'{ts_name}{in_ext}'
+            output = f'{ts_name}_norm.mrc'
+
+            # normalize to mean 0 and stdev 1
+            with (
+                mrcfile.open(input) as mrc,
+                mrcfile.new(output, overwrite=overwrite) as mrc_norm
+            ):
+                mrc_norm.set_data((mrc.data - mrc.data.mean()) / mrc.data.std())
+
+
+def run_align(imod_dir, ts_list, overwrite, in_ext, aretomo, tilt_axis, warp_dir):
     gpus = [gpu.id for gpu in GPUtil.getGPUs()]
     if not gpus:
         raise click.UsageError('you need at least one GPU to run AreTomo')
 
-    warp_dir = Path(warp_dir)
-    imod_dir = warp_dir / 'imod'
-    if not imod_dir.exists():
-        raise click.UsageError('warp directory does not have an `imod` subdirectory')
-
-    ts_list = sorted(list(imod_dir.iterdir()))
-    if dry_run:
-        newline = '\n'
-        click.secho(cleandoc(f'''
-            Warp directory:
-                - {warp_dir}
-            Tilt series found:{''.join(f'{newline}{" " * 16}- {ts.stem}' for ts in ts_list)}
-        '''))
-        click.get_current_context().exit()
-
-    def check_output_exists(ts_name):
-        for ts_dir in ts_list:
-            file = Path(f'{ts_dir / ts_dir.stem}.mrc.xf')
-            if file.exists() and not overwrite:
-                raise FileExistsError(f'{file} already exists; use -f/--overwrite to overwrite any existing files')
-
-    with click.progressbar(ts_list, label='Fixing...     ', item_show_func=lambda x: str(x.stem if x is not None else '')) as bar:
+    warn = []
+    with click.progressbar(ts_list, label='Aligning...   ', item_show_func=str) as bar:
         for ts_dir in bar:
             ts_name = ts_dir / ts_dir.stem
-            raw = f'{ts_name}.mrc.st'
-            fixed = f'{ts_name}_fixed.mrc'
-
-            # run ccderaser, defaults from etomo
-            ccderaser_cmd = f'{ccderaser} -input {raw} -output {fixed} -find -peak 8.0 -diff 6.0 -big 19. -giant 12. -large 8. -grow 4. -edge 4'
-            subprocess.run(ccderaser_cmd.split(), capture_output=True, check=True,)
-
-    with click.progressbar(ts_list, label='Normalizing...', item_show_func=lambda x: str(x.stem if x is not None else '')) as bar:
-        for ts_dir in bar:
-            fixed = f'{ts_name}_fixed.mrc'
-            normalized = f'{ts_name}_norm.mrc'
-
-            # normalize to mean 0 and stdev 1
-            with (
-                mrcfile.open(fixed) as mrc,
-                mrcfile.new(normalized, overwrite=overwrite) as mrc_norm
-            ):
-                mrc_norm.set_data((mrc.data - mrc.data.mean()) / mrc.data.std())
-
-    warn = []
-    with click.progressbar(ts_list, label='Aligning...   ', item_show_func=lambda x: str(x.stem if x is not None else '')) as bar:
-        for ts_dir in bar:
-            normalized = f'{ts_name}_norm.mrc'
+            input = f'{ts_name}{in_ext}'
             aligned = f'{ts_name}_aligned.mrc'
             tilt = f'{ts_name}.mrc.rawtlt'
             xf = f'{ts_name}.xf'
@@ -83,7 +70,7 @@ def main(warp_dir, dry_run, ccderaser, aretomo, tilt_axis, overwrite):
 
             # run aretomo with basic settings
             tilt_axis_opt = f'-TiltAxis {tilt_axis}' if tilt_axis is not None else ''
-            aretomo_cmd = f'{aretomo} -InMrc {normalized} -OutMrc {aligned} -AngFile {tilt} -OutXF 1 {tilt_axis_opt} -TiltCor 1 -Gpu {gpus[0]} -VolZ 0'
+            aretomo_cmd = f'{aretomo} -InMrc {input} -OutMrc {aligned} -AngFile {tilt} -OutXF 1 {tilt_axis_opt} -TiltCor 1 -Gpu {gpus[0]} -VolZ 0'
             proc = subprocess.run(aretomo_cmd.split(), capture_output=True, check=True)
 
             # rename so warp sees it
@@ -115,3 +102,50 @@ def main(warp_dir, dry_run, ccderaser, aretomo, tilt_axis, overwrite):
     if warn:
         click.secho(f'WARNING: somehow found the wrong number of xml files in {warp_dir}')
         click.secho(f'Check the log and manually disable tilts in Warp for: {", ".join(warn)}.')
+
+
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.argument('warp_dir', type=click.Path(exists=True, dir_okay=True, resolve_path=True), default='.')
+@click.option('-d', '--dry-run', is_flag=True, help='only print some info, without running the commands')
+@click.option('-t', '--tilt-axis', type=float, help='starting tilt axis for AreTomo, if any')
+@click.option('-f', '--overwrite', is_flag=True, help='overwrite any previous existing run')
+@click.option('--fix/--nofix', default=True, help='run ccderaser to fix the stack')
+@click.option('--norm/--nonorm', default=True, help='run ccderaser to fix the stack')
+@click.option('--align/--noalign', default=True, help='run ccderaser to fix the stack')
+@click.option('--ccderaser', type=str, default='ccderaser', help='command for ccderaser')
+@click.option('--aretomo', type=str, default='AreTomo', help='command for aretomo')
+def main(warp_dir, dry_run, ccderaser, aretomo, tilt_axis, overwrite, fix, norm, align):
+    """
+    run aretomo on a warp directory (after imod stacks were generated).
+    Requires ccderaser and AreTomo.
+
+    Assumes the default Warp directory structure with generated imod stacks
+    """
+    warp_dir = Path(warp_dir)
+    imod_dir = warp_dir / 'imod'
+    if not imod_dir.exists():
+        raise click.UsageError('warp directory does not have an `imod` subdirectory')
+
+    ts_list = sorted([ts.stem for ts in imod_dir.iterdir()])
+    if dry_run:
+        newline = '\n'
+        click.secho(cleandoc(f'''
+            Warp directory:
+                - {warp_dir}
+            Tilt series found:{''.join(f'{newline}{" " * 16}- {ts}' for ts in ts_list)}
+        '''))
+        click.get_current_context().exit()
+
+    input_ext = '.mrc.st'
+    if fix:
+        run_fix(imod_dir, ts_list, overwrite, input_ext, ccderaser)
+        input_ext = '_fixed.mrc'
+
+    if norm:
+        run_normalize(imod_dir, ts_list, overwrite, input_ext)
+        input_ext = '_norm.mrc'
+
+    if align:
+        run_align(imod_dir, ts_list, overwrite, input_ext, aretomo, tilt_axis, warp_dir)
+
+    click.secho('Done! Results are in the respective imod directories.')
