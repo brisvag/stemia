7c316c4157dd9c8357f75505e920b2338a5f1456
Lorenzo Gaifas <brisvag@gmail.com>
2022-03-08 14:10:37 +0100

    cool stuff

--- stemia/aretomo_align.py
+++ stemia/aretomo_align.py
@@ -3,19 +3,25 @@ import click
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.argument('warp_dir', type=click.Path(exists=True, dir_okay=True, resolve_path=True), default='.')
+@click.option('-d', '--dry-run', is_flag=True, help='only print some info, without running the commands')
+@click.option('-t', '--tilt-axis', type=str, help='starting tilt axis for AreTomo, if any')
+@click.option('-f', '--overwrite', is_flag=True, help='overwrite any previous existing run')
 @click.option('--ccderaser', type=str, default='ccderaser', help='command for ccderaser')
 @click.option('--aretomo', type=str, default='AreTomo', help='command for aretomo')
-@click.option('--tilt_axis', type=str, help='starting tilt axis for AreTomo, if any')
-def main(warp_dir, ccderaser, aretomo, tilt_axis):
+def main(warp_dir, dry_run, ccderaser, aretomo, tilt_axis, overwrite):
     """
     run aretomo on a warp directory (after imod stacks were generated).
     Requires ccderaser and AreTomo.
+
+    Assumes the default Warp directory structure with generated imod stacks
     """
     import subprocess
     from pathlib import Path
     import re
     import shutil
     import GPUtil
+    from inspect import cleandoc
+    import mrcfile
 
     if not shutil.which(ccderaser):
         raise click.UsageError('ccderaser is not available on the system')
@@ -25,41 +31,76 @@ def main(warp_dir, ccderaser, aretomo, tilt_axis):
 
     warp_dir = Path(warp_dir)
     imod_dir = warp_dir / 'imod'
+    if not imod_dir.exists():
+        raise click.UsageError('warp directory does not have an `imod` subdirectory')
+
+    ts_list = sorted(list(imod_dir.iterdir()))
+    if dry_run:
+        newline = '\n'
+        click.secho(cleandoc(f'''
+            Warp directory:
+                - {warp_dir}
+            Tilt series found:{''.join(f'{newline}{" " * 16}- {ts.stem}' for ts in ts_list)}
+        '''))
+        click.get_current_context().exit()
 
     warn = []
-    with click.progressbar(list(imod_dir.iterdir()), label='Aligning...', item_show_func=lambda x: x.stem) as bar:
+    with click.progressbar(ts_list, item_show_func=lambda x: str(x)) as bar:
         for ts_dir in bar:
             ts_name = ts_dir / ts_dir.stem
+            raw = f'{ts_name}.mrc.st'
+            tilt = f'{ts_name}.mrc.rawtlt'
+            fixed = f'{ts_name}_fixed.mrc'
+            normalized = f'{ts_name}_norm.mrc'
+            aligned = f'{ts_name}_aligned.mrc'
+            xf = f'{ts_name}.xf'
+            xf_warp = f'{ts_name}.mrc.xf'  # needs to be like this for warp to see it
+
+            for file in (fixed, normalized, aligned, xf):
+                if Path(file).exists() and not overwrite:
+                    raise FileExistsError(f'{file} already exists; use -f/--overwrite to overwrite any existing files')
+
             # run ccderaser, defaults from etomo
-            ccderaser_cmd = f'{ccderaser} -input {ts_name}.mrc.st -output {ts_name}.mrc_fixed.st -find -peak 8.0 -diff 6.0 -big 19. -giant 12. -large 8. -grow 4. -edge 4'
+            ccderaser_cmd = f'{ccderaser} -input {raw} -output {fixed} -find -peak 8.0 -diff 6.0 -big 19. -giant 12. -large 8. -grow 4. -edge 4'
             subprocess.run(ccderaser_cmd.split(), check=True)
 
-            # run aretomo with
+            # normalize to mean 0 and stdev 1
+            with (
+                mrcfile.open(fixed) as mrc,
+                mrcfile.new(normalized) as mrc_norm
+            ):
+                mrc_norm.set_data((mrc.data - mrc.data.mean()) / mrc.data.std())
+
+            # run aretomo with basic settings
             tilt_axis_opt = f'-TiltAxis {tilt_axis}' if tilt_axis is not None else ''
-            aretomo_cmd = f'{aretomo} -InMrc {ts_name}.mrc_fixed.st -OutMrc {ts_name}_aretomo.mrc -AngFile {ts_name}.mrc.rawtlt -OutXF 1 {tilt_axis_opt} -TiltCor 1 -Gpu {gpus[0]} -VolZ 0'
+            aretomo_cmd = f'{aretomo} -InMrc {normalized} -OutMrc {aligned} -AngFile {tilt} -OutXF 1 {tilt_axis_opt} -TiltCor 1 -Gpu {gpus[0]} -VolZ 0'
             proc = subprocess.run(aretomo_cmd.split(), capture_output=True, check=True)
 
-            # find removed images
+            # rename so warp sees it
+            Path(xf).rename(xf_warp)
+
+            # find any images removed by aretomo cause too dark (should not happen with normalization)
             to_skip = []
-            for line in proc.stdout.split('\n'):
-                if match := re.search(r'Remove image at (\S+) degree'):
+            for line in proc.stdout.decode().split('\n'):
+                if match := re.search(r'Remove image at (\S+) degree', line):
                     to_skip.append(match.group(1))
 
-            log = ts_dir / 'aretomo_align.log'
-            skipped = "\n".join([angle for angle in to_skip])
-            log.write_text(f'Some images were too dark and were skipped by AreTomo:\n{skipped}')
-
-            # flag as disabled in warp
-            for angle in to_skip:
-                glob = f'{ts_name}_*_{angle}.xml'
-                xml = list(warp_dir.glob(glob))
-                if len(xml) != 1:
-                    warn.append(ts_name)
-                    continue
-                xml = xml[0]
-                content = xml.read_text()
-                replaced = content.replace('UnselectManual="null"', 'UnselectManual="True"')
-                xml.write_text(replaced)
+            if to_skip:
+                log = ts_dir / 'aretomo_align.log'
+                skipped = "\n".join([angle for angle in to_skip])
+                log.write_text(f'Some images were too dark and were skipped by AreTomo:\n{skipped}')
+                # flag as disabled in warp
+                for angle in to_skip:
+                    glob = f'{ts_name}_*_{angle}.xml'
+                    xml = list(warp_dir.glob(glob))
+                    if len(xml) != 1:
+                        warn.append(ts_name)
+                        continue
+                    xml = xml[0]
+                    content = xml.read_text()
+                    replaced = content.replace('UnselectManual="null"', 'UnselectManual="True"')
+                    click.secho(f'doing stuff with {xml}')
+                    xml.write_text(replaced)
 
     if warn:
         click.secho(f'WARNING: somehow found the wrong number of xml files in {warp_dir}')
