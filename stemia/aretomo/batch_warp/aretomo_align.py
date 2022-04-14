4f8fa2995ab98231b605974d7e409cd289ccb86f
Lorenzo Gaifas <brisvag@gmail.com>
2022-04-14 11:24:27 +0200

    workable

--- stemia/aretomo/batch_warp/aretomo_align.py
+++ stemia/aretomo/batch_warp/aretomo_align.py
@@ -3,20 +3,24 @@ import click
 
 @click.command()
 @click.argument('warp_dir', type=click.Path(exists=True, dir_okay=True, resolve_path=True))
+@click.option('-m', '--mdoc-dir', type=click.Path(exists=True, dir_okay=True, resolve_path=True))
 @click.option('-d', '--dry-run', is_flag=True, help='only print some info, without running the commands')
-@click.option('-t', '--sample-thickness', type=float, default=1200,
-              help='unbinned thickness of the SAMPLE; the reconstruction will be 20% thicker, but this will be used for alignment')
-@click.option('-b', '--binning', type=int, help='binning for aretomo reconstruction')
+@click.option('-o', '--only', type=str, multiple=True,
+              help='quickly reconstruct just this tomogram with a simple setup. Useful for testing and to estimate sample thickness')
+@click.option('-t', '--thickness', type=int, default=1200,
+              help='unbinned thickness of the SAMPLE (ice or lamella); the reconstruction will be 20% thicker, but this will be used for alignment')
+@click.option('-b', '--binning', type=int, default=4, help='binning for aretomo reconstruction')
 @click.option('-a', '--tilt-axis', type=float, help='starting tilt axis for AreTomo, if any')
+@click.option('-p', '--patches', type=int, default=4, help='number of patches for local alignment in aretomo (NxN)')
 @click.option('-f', '--overwrite', is_flag=True, help='overwrite any previous existing run')
 @click.option('--fix/--nofix', default=True, help='run ccderaser to fix the stack')
-@click.option('--norm/--nonorm', default=False, help='use mrcfile to normalize the images')
+@click.option('--norm/--nonorm', default=True, help='use mrcfile to normalize the images')
 @click.option('--align/--noalign', default=True, help='run aretomo to produce an alignment')
-@click.option('--startfrom', type=click.Choice(('raw', 'fix', 'norm')), default='raw',
-              help='use outputs from a previous run starting from this step')
+@click.option('--startfrom', type=click.Choice(('auto', 'fix', 'norm', 'ali')), default='auto',
+              help='use outputs from a previous run, starting processing at this step')
 @click.option('--ccderaser', type=str, default='ccderaser', help='command for ccderaser')
 @click.option('--aretomo', type=str, default='AreTomo', help='command for aretomo')
-def cli(warp_dir, dry_run, sample_thickness, binning, tilt_axis, overwrite, fix, norm, align, startfrom, ccderaser, aretomo):
+def cli(warp_dir, mdoc_dir, dry_run, only, thickness, binning, tilt_axis, patches, overwrite, fix, norm, align, startfrom, ccderaser, aretomo):
     """
     Run aretomo in batch on data preprocessed in warp.
 
@@ -35,7 +39,12 @@ def cli(warp_dir, dry_run, sample_thickness, binning, tilt_axis, overwrite, fix,
     if not imod_dir.exists():
         raise click.UsageError('warp directory does not have an `imod` subdirectory')
 
-    mdocs = sorted(list(warp_dir.glob('*.mdoc')))
+    if mdoc_dir is None:
+        mdoc_dir = warp_dir
+    if only is not None:
+        mdocs = [Path(mdoc_dir) / (ts_name + '.mdoc') for ts_name in only]
+    else:
+        mdocs = sorted(list(Path(mdoc_dir).glob('*.mdoc')))
     if not mdocs:
         raise click.UsageError('could not find any mdoc files')
 
@@ -44,11 +53,20 @@ def cli(warp_dir, dry_run, sample_thickness, binning, tilt_axis, overwrite, fix,
     if not (even_dir := warp_dir / 'average' / 'even').exists():
         raise click.UsageError('could not find odd/even averages')
 
+    recon_thickness = int(thickness * 1.3)
+
     tilt_series = {}
+    tilt_series_unprocessed = {}
     with click.progressbar(mdocs, label='Reading metadata...', item_show_func=lambda x: x.stem if x is not None else None) as bar:
         for mdoc in bar:
             df = mdocfile.read(mdoc)
             ts_name = df.image_file[0].name
+            ts_dir = imod_dir / ts_name
+
+            # skip if not preprocessed in warp
+            if not ts_dir.is_dir():
+                tilt_series_unprocessed.append(ts_name)
+                continue
 
             # extract frame paths
             frames = [warp_dir / PureWindowsPath(fr).name for fr in df.sub_frame_path]
@@ -65,53 +83,76 @@ def cli(warp_dir, dry_run, sample_thickness, binning, tilt_axis, overwrite, fix,
                     voltage = int(param.get('Value'))
                 elif param.get('Name') == 'Cs':
                     cs = float(param.get('Value'))
+                elif param.get('Name') == 'Kv':
+                    kv = int(param.get('Value'))
             for param in xml.find('CTF'):
                 if param.get('Name') == 'Defocus':
                     defocus = float(param.get('Value'))
 
             tilt_series[ts_name] = {
+                'dir': ts_dir,
                 'frames': frames,
-                'dose': df.exposure_dose[0],
-                'px_size': df.pixel_spacing[0] * 2**bin,
-                'voltage': voltage,
-                'cs': cs,
-                'defocus': defocus,
+                'aretomo_kwargs': {
+                    'dose': df.exposure_dose[0],
+                    'px_size': df.pixel_spacing[0] * 2**bin,
+                    'voltage': voltage,
+                    'cs': cs,
+                    'kv': kv,
+                    'defocus': defocus,
+                },
             }
 
-    ts_dirs = sorted([dir for ts_name in tilt_series if (dir := imod_dir / ts_name).is_dir()])
-
-    # if dry_run:
-    if True:
-        newline = '\n'
-        click.secho(cleandoc(f'''
-            Warp directory: {warp_dir}
-            Tilt series:{''.join(f'{newline}{" " * 16}- {ts.stem}' for ts in ts_dirs)}
-            AreTomo options:
-                - unbinned thickness: {sample_thickness}
-                - tomogram binning: {binning}
-        '''))
+    newline = '\n'
+    click.secho(cleandoc(f'''
+        Warp directory: {warp_dir}
+        Mdoc directory: {mdoc_dir}
+        Tilt series:{''.join(f'{newline}{" " * 12}- {ts.stem}' for ts in ts_dirs)}
+        AreTomo options:
+            - alignment thickness: {thickness}
+            - reconstruction thickness: {recon_thickness}
+            - tomogram binning: {binning}
+    '''))
+    if dry_run:
         click.get_current_context().exit()
 
-    from .funcs import run_fix, run_normalize, run_align
+    from .funcs import run_fix, run_normalize, run_align, outputs_exist
+
+    ts_dirs = list(ts['dir'] for ts in tilt_series.values())
+
+    if startfrom == 'auto':
+        if outputs_exist(ts_dirs, '.fixed'):
+            startfrom = 'norm'
+        if outputs_exist(ts_dirs, '.norm'):
+            startfrom = 'ali'
+        if outputs_exist(ts_dirs, '.aligned'):
+            raise click.UsageError('all outputs appear to exist. Do you need to change some parameter?')
 
     input_ext = '.st'
-    if startfrom == 'fix':
-        input_ext = '_fixed.mrc'
+    if startfrom == 'norm':
+        input_ext = '.fixed'
         fix = False
-    elif startfrom == 'norm':
-        input_ext == '_norm.mrc'
+    elif startfrom == 'ali':
+        input_ext = '.norm'
         fix = False
         norm = False
 
     if fix:
-        run_fix(ts_list, overwrite, input_ext, ccderaser)
-        input_ext = '_fixed.mrc'
+        run_fix(ts_dirs, overwrite, input_ext, cmd=ccderaser)
+        input_ext = '.fixed'
 
     if norm:
-        run_normalize(ts_list, overwrite, input_ext)
-        input_ext = '_norm.mrc'
+        run_normalize(ts_dirs, overwrite, input_ext)
+        input_ext = '.norm'
 
     if align:
-        run_align(ts_list, overwrite, input_ext, aretomo, tilt_axis)
-
-    run_align(ts_dirs, overwrite, aretomo, tilt_axis)
+        run_align(
+            tilt_series,
+            overwrite,
+            input_ext,
+            cmd=aretomo,
+            tilt_axis=tilt_axis,
+            patches=patches,
+            thickness_align=thickness,
+            thickness_recon=recon_thickness,
+            binning=binning),
+        input_ext = '.aligned'
--- stemia/aretomo/batch_warp/funcs.py
+++ stemia/aretomo/batch_warp/funcs.py
@@ -4,16 +4,21 @@ import re
 import shutil
 import GPUtil
 import mrcfile
+from queue import Queue
+from concurrent import futures
 
 
-def check_outputs_exist(dirs, ext):
+def outputs_exist(ts_dirs, ext, error=False):
     if not isinstance(ext, tuple):
         ext = (ext,)
-    for dir in dirs:
+    for ts_dir in ts_dirs:
         for ex in ext:
-            file = dir / f'{dir.stem}{ext}'
+            file = ts_dir / (ts_dir.name + ex)
             if file.exists():
-                raise FileExistsError(f'{file} already exists; use -f/--overwrite to overwrite any existing files')
+                if error:
+                    raise FileExistsError(f'{file} already exists; use -f/--overwrite to overwrite any existing files')
+                return True
+    return False
 
 
 def get_stem(path):
@@ -22,113 +27,169 @@ def get_stem(path):
     return ''
 
 
-def run_fix(ts_list, overwrite, in_ext, ccderaser):
-    if not shutil.which(ccderaser):
+def _ccderaser(input, cmd='ccderaser'):
+    fixed = input.with_suffix('.fixed')
+    # run ccderaser, defaults from etomo
+    ccderaser_cmd = f'{cmd} -input {input} -output {fixed} -find -peak 8.0 -diff 6.0 -big 19. -giant 12. -large 8. -grow 4. -edge 4'
+    subprocess.run(ccderaser_cmd.split(), capture_output=True, check=True)
+
+
+def run_fix(ts_dirs, overwrite, in_ext, cmd='ccderaser'):
+    if not shutil.which(cmd):
         raise click.UsageError('ccderaser is not available on the system')
     if not overwrite:
-        check_outputs_exist(ts_list, '_fixed.mrc')
-    with click.progressbar(ts_list, label='Fixing...     ', item_show_func=get_stem) as bar:
-        for ts_dir in bar:
-            ts_name = ts_dir / ts_dir.stem
-            input = f'{ts_name}{in_ext}'
-            output = f'{ts_name}_fixed.mrc'
+        outputs_exist(ts_dirs, '.fixed', error=True)
+    with futures.ThreadPoolExecutor() as executor:
+        jobs = []
+        for ts_dir in ts_dirs:
+            input = ts_dir / (ts_dir.name + in_ext)
+            jobs.append(executor.submit(_ccderaser, input, cmd=cmd))
+        with click.progressbar(length=len(ts_dirs), label='Fixing...') as bar:
+            for job in futures.as_completed(jobs):
+                bar.update(1)
 
-            # run ccderaser, defaults from etomo
-            ccderaser_cmd = f'{ccderaser} -input {input} -output {output} -find -peak 8.0 -diff 6.0 -big 19. -giant 12. -large 8. -grow 4. -edge 4'
-            subprocess.run(ccderaser_cmd.split(), capture_output=True, check=True)
+
+def _normalize(input):
+    normalized = input.with_suffix('.norm')
+    # normalize to mean 0 and stdev 1
+    with (
+        mrcfile.open(input) as mrc,
+        mrcfile.new(normalized, overwrite=True) as mrc_norm
+    ):
+        mrc_norm.set_data((mrc.data - mrc.data.mean()) / mrc.data.std())
 
 
-def run_normalize(ts_list, overwrite, in_ext):
+def run_normalize(ts_dirs, overwrite, in_ext):
     if not overwrite:
-        check_outputs_exist(ts_list, '_norm.mrc')
-    with click.progressbar(ts_list, label='Normalizing...', item_show_func=get_stem) as bar:
-        for ts_dir in bar:
-            ts_name = ts_dir / ts_dir.stem
-            input = f'{ts_name}{in_ext}'
-            output = f'{ts_name}_norm.mrc'
+        outputs_exist(ts_dirs, '.norm', error=True)
+    with futures.ThreadPoolExecutor() as executor:
+        jobs = []
+        for ts_dir in ts_dirs:
+            input = ts_dir / (ts_dir.name + in_ext)
+            jobs.append(executor.submit(_normalize, input))
+        with click.progressbar(length=len(ts_dirs), label='Normalizing...') as bar:
+            for job in futures.as_completed(jobs):
+                bar.update(1)
 
-            # normalize to mean 0 and stdev 1
-            with (
-                mrcfile.open(input) as mrc,
-                mrcfile.new(output, overwrite=overwrite) as mrc_norm
-            ):
-                mrc_norm.set_data((mrc.data - mrc.data.mean()) / mrc.data.std())
 
-def aretomo_wrapper(ts_dir, in_ext, tilt_axis=0, patches=0, thickness_recon=0, gpu=0):
+def _aretomo(
+    input,
+    cmd='AreTomo',
+    gpu=0,
+    tilt_axis=0,
+    patches=0,
+    thickness_align=1200,
+    thickness_recon=0,
+    binning=4,
+    px_size=0,
+    kv=0,
+    dose=0,
+    cs=0,
+    defocus=0,
+):
+    output = input.with_suffix('.aligned')
+    rawtlt = input.with_suffix('.rawtlt')
+    log = input.with_suffix('.log')
+    cwd = output.parent.absolute()
     options = {
-        'InMrc': input,
-        'OutMrc': aligned,
-        'AngFile': rawtilt,
-        'TiltAxis': f'{tilt_axis}',
+        'InMrc': input.relative_to(cwd),
+        'OutMrc': output.relative_to(cwd),
+        'AngFile': rawtlt.relative_to(cwd),
+        # 'LogFile': log.relative_to(cwd),  # currently broken
+        'TiltAxis': tilt_axis or 0,
         'Patch': f'{patches} {patches}',
+        'AlignZ': thickness_align,
         'VolZ': thickness_recon,
+        'OutBin': binning,
+        'PixSize': px_size,
+        'Kv': kv,
+        'ImgDose': dose,
+        'Cs': cs,
+        'Defoc': defocus,
         'Gpu': gpu,
         'OutXF': 1,
-        'DarlTol': 0.5,
         'TiltCor': 1,
+        'FlipVol': 1,
+        'DarkTol': 0,
     }
     # run aretomo with basic settings
-    aretomo_cmd = ' '.join(f'-{k} {v}' for k, v in options.items())
-    print(aretomo_cmd)
-    # return subprocess.run(aretomo_cmd.split(), capture_output=True, check=True)
+    aretomo_cmd = f"{cmd} {' '.join(f'-{k} {v}' for k, v in options.items())}"
+    proc = subprocess.run(aretomo_cmd.split(), capture_output=True, check=False, cwd=cwd)
+    log.write_bytes(proc.stdout + proc.stderr)
 
 
-def run_align(ts_dirs, overwrite, in_ext, aretomo, tilt_axis):
+def _aretomo_queue(*args, gpu_queue=None, **kwargs):
+    gpu = gpu_queue.get()
+    try:
+        _aretomo(*args, **kwargs, gpu=gpu)
+    finally:
+        gpu_queue.put(gpu)
+
+
+def run_align(ts_dict, overwrite, in_ext, **aretomo_kwargs):
     gpus = [gpu.id for gpu in GPUtil.getGPUs()]
     if not gpus:
         raise click.UsageError('you need at least one GPU to run AreTomo')
+    click.secho(f'Running AreTomo in parallel on {len(gpus)} GPUs.')
+    # use a queue to hold gpu ids to ensure we only run one job per gpu
+    gpu_queue = Queue()
+    for gpu in gpus:
+        gpu_queue.put(gpu)
+
     if not overwrite:
-        check_outputs_exist(ts_dirs, ('_aligned.mrc', 'mrc.xf'))
+        outputs_exist((ts['dir'] for ts in ts_dict.values()), ('.aligned', '.xf'), error=True)
+
     warn = []
     skipped = False
-    with click.progressbar(ts_dirs, label='Aligning...   ', item_show_func=get_stem) as bar:
-        for ts_dir in bar:
-            input = ts_dir / (ts_dir.stem + in_ext)
-            aligned = input.with_stem('.aligned')
-            rawtilt = input.with_stem('.rawtilt')
+    with futures.ThreadPoolExecutor(len(gpus)) as executor:
+        jobs = []
+        for ts_name, ts_data in ts_dict:
+            input = ts_data['dir'] / (ts_name + in_ext)
+            jobs.append(executor.submit(_aretomo_queue, input, **aretomo_kwargs, **ts_data['aretomo_kwargs'], gpu_queue=gpu_queue))
+        with click.progressbar(length=len(ts_dict), label='Aligning...') as bar:
+            for job in futures.as_completed(jobs):
+                bar.update(1)
 
-            proc = aretomo_wrapper()
+            # # aretomo is somehow circumventing the `cwd` argument of subprocess.run and dumping everything in the PARENT
+            # # of the actual cwd. Could not solve, no clue why this happens. So we have to do things differently
+            # # and move the output around a bit
+            # imod_dir = ts_dir.parent
+            # tlt = imod_dir / f'{ts_dir.stem}.aln'
+            # aln = imod_dir / f'{ts_dir.stem}.tlt'
+            # xf = imod_dir / f'{ts_dir.stem}.xf'
+            # xf_warp = f'{ts_name}.mrc.xf'  # needs to be renamed for warp to see it
+            # shutil.move(tlt, ts_dir)
+            # shutil.move(aln, ts_dir)
+            # shutil.move(xf, xf_warp)
 
-            # aretomo is somehow circumventing the `cwd` argument of subprocess.run and dumping everything in the PARENT
-            # of the actual cwd. Could not solve, no clue why this happens. So we have to do things differently
-            # and move the output around a bit
-            imod_dir = ts_dir.parent
-            tlt = imod_dir / f'{ts_dir.stem}.aln'
-            aln = imod_dir / f'{ts_dir.stem}.tlt'
-            xf = imod_dir / f'{ts_dir.stem}.xf'
-            xf_warp = f'{ts_name}.mrc.xf'  # needs to be renamed for warp to see it
-            shutil.move(tlt, ts_dir)
-            shutil.move(aln, ts_dir)
-            shutil.move(xf, xf_warp)
+            # # find any images removed by aretomo cause too dark (should not happen with normalization)
+            # to_skip = []
+            # for line in proc.stdout.decode().split('\n'):
+                # if match := re.search(r'Remove image at (\S+) degree: .*', line):
+                    # to_skip.append(match)
 
-            # find any images removed by aretomo cause too dark (should not happen with normalization)
-            to_skip = []
-            for line in proc.stdout.decode().split('\n'):
-                if match := re.search(r'Remove image at (\S+) degree: .*', line):
-                    to_skip.append(match)
+            # if to_skip:
+                # skipped = True
+                # warp_dir = ts_dirs[0].parent.parent
+                # log = ts_dir / 'aretomo_align.log'
+                # skipped = "\n".join([match.group() for match in to_skip])
+                # log.write_text(f'Some images were too dark and were skipped by AreTomo:\n{skipped}\n')
+                # # flag as disabled in warp
+                # for match in to_skip:
+                    # angle = match.group(1)
+                    # glob = f'{ts_dir.stem}_*_{float(angle):.1f}.xml'
+                    # xml = list(warp_dir.glob(glob))
+                    # if len(xml) != 1:
+                        # warn.append(ts_name)
+                        # continue
+                    # xml = xml[0]
+                    # content = xml.read_text(encoding='utf16')  # warp...
+                    # replaced = content.replace('UnselectManual="null"', 'UnselectManual="True"')
+                    # xml.write_text(replaced, encoding='utf16')
 
-            if to_skip:
-                skipped = True
-                warp_dir = ts_dirs[0].parent.parent
-                log = ts_dir / 'aretomo_align.log'
-                skipped = "\n".join([match.group() for match in to_skip])
-                log.write_text(f'Some images were too dark and were skipped by AreTomo:\n{skipped}\n')
-                # flag as disabled in warp
-                for match in to_skip:
-                    angle = match.group(1)
-                    glob = f'{ts_dir.stem}_*_{float(angle):.1f}.xml'
-                    xml = list(warp_dir.glob(glob))
-                    if len(xml) != 1:
-                        warn.append(ts_name)
-                        continue
-                    xml = xml[0]
-                    content = xml.read_text(encoding='utf16')  # warp...
-                    replaced = content.replace('UnselectManual="null"', 'UnselectManual="True"')
-                    xml.write_text(replaced, encoding='utf16')
-
-    if warn:
-        click.secho(f'WARNING: somehow found the wrong number of xml files in {warp_dir}')
-        click.secho(f'Check the log and manually disable tilts in Warp for: {", ".join(str(ts)) for ts in warn}.')
-    elif skipped:
-        click.secho('WARNING: some images were too dark and were skipped by aretomo.')
-        click.secho('Warp xml files were automatically updated to reflect these changes by skipping some images!')
+    # if warn:
+        # click.secho(f'WARNING: somehow found the wrong number of xml files in {warp_dir}')
+        # click.secho(f'Check the log and manually disable tilts in Warp for: {", ".join(str(ts)) for ts in warn}.')
+    # elif skipped:
+        # click.secho('WARNING: some images were too dark and were skipped by aretomo.')
+        # click.secho('Warp xml files were automatically updated to reflect these changes by skipping some images!')
