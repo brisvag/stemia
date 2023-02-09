eaf9fae0c462dbe3a0aa8570f2452bd9ac49dd9b
Lorenzo Gaifas <brisvag@gmail.com>
2023-02-09 15:24:22 +0100

    fix issues with fix_mdoc

--- stemia/warp/fix_mdoc.py
+++ stemia/warp/fix_mdoc.py
@@ -2,8 +2,9 @@ import click
 
 
 @click.command()
-@click.argument('data_dir', type=click.Path(exists=True, dir_okay=True, resolve_path=True))
-def cli(data_dir):
+@click.argument('mdoc_dir', type=click.Path(exists=True, dir_okay=True, resolve_path=True))
+@click.option('-d', '--data-dir', type=click.Path(exists=True, dir_okay=True, resolve_path=True))
+def cli(mdoc_dir, data_dir):
     """
     Fix mdoc files to point to the right data.
     """
@@ -12,33 +13,35 @@ def cli(data_dir):
     from rich.progress import track
     from rich import print
 
+    mdoc_dir = Path(mdoc_dir)
     data_dir = Path(data_dir)
-    mdocs = [f for f in data_dir.glob('*.mdoc') if not f.stem.endswith('_fixed')]
+    mdocs = [f for f in mdoc_dir.glob('*.mdoc') if not f.stem.endswith('_fixed')]
     failed = {}
-    with track(mdocs, description='Fixing...') as bar:
-        for md_file in bar:
-            mdoc = Mdoc.from_file(md_file)
-            basename = mdoc.global_data.ImageFile.stem
-            cleaned_sections = []
-            for section in mdoc.section_data:
-                # find correct file based on basename, zvalue and tilt angle
-                glob = f'{basename}_{section.ZValue + 1:03}_{round(section.TiltAngle):.2f}_*.mrc'
-                try:
-                    newpath = next(data_dir.glob(glob))
-                except StopIteration:
-                    failed.setdefault(mdoc, list).append(glob)
-                    continue
-                # replace path with the correct file (only name matters)
-                section.SubFramePath = fr'X:\spoof\frames\{newpath.name}'
-                cleaned_sections.append(section)
+    for md_file in track(mdocs, description='Fixing...'):
+        mdoc = Mdoc.from_file(md_file)
+        basename = mdoc.global_data.ImageFile.stem
+        cleaned_sections = []
+        for section in mdoc.section_data:
+            # find correct file based on basename, zvalue and tilt angle
+            glob = f'{basename}_{section.ZValue + 1:03}_{round(section.TiltAngle):.2f}_*.mrc'
 
-            mdoc.section_data = cleaned_sections
+            try:
+                newpath = next(data_dir.glob(glob))
+            except StopIteration:
+                failed.setdefault(md_file, list()).append(glob)
+                continue
 
-        if failed:
-            print('No files matching the followings sections:')
-            for k, v in failed:
-                print(k.name, ':')
-                print('\n- '.join(v))
+            # replace path with the correct file (only name matters)
+            section.SubFramePath = fr'X:\spoof\frames\{newpath.name}'
+            cleaned_sections.append(section)
 
-            with open(md_file.parent / (md_file.stem + '_fixed.mdoc'), 'w+') as f:
-                f.write(mdoc.to_string())
+        mdoc.section_data = cleaned_sections
+
+        with open(md_file.parent / (md_file.stem + '_fixed.mdoc'), 'w+') as f:
+            f.write(mdoc.to_string())
+
+    if failed:
+        print('No files matching the followings sections:')
+        for k, v in failed.items():
+            print(k.name, ':')
+            print('\n- '.join(v))
